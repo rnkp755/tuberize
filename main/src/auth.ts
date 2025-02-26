807437c4d35bad775d49a3ab8f3bdc76b5a9f3e7
RAUSHAN KUMAR THAKUR <rnkp755@gmail.com>
2025-02-26 16:15:10 +0530

    Error is gone but user isn't being saved to db

--- main/src/auth.ts
+++ main/src/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../prisma/prisma";
 import client from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	adapter: MongoDBAdapter(client),
+	adapter: PrismaAdapter(prisma),
 	providers: [Google, GitHub],
 });
